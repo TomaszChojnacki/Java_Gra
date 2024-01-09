e7ec10bf49275f8716883e6ddc14375b0cedbc2f
Tomasz Chojnacki <tomaszchojnacki3@gmail.com>
2024-01-09 20:47:24 +0100

    initial commit

--- src/main/java/org/example/MenuZTlemObrazu.java
+++ src/main/java/org/example/MenuZTlemObrazu.java
@@ -8,7 +8,7 @@ import java.awt.event.ActionListener;
 public class MenuZTlemObrazu extends JFrame {
 
     public MenuZTlemObrazu() {
-        // Ustawienia okna
+        // Ustawienia okn
         setTitle("Bomberman");
         setSize(800, 800);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
